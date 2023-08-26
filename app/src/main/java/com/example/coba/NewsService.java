a7f278954e4fe0232b54844a4578558bdfadc8a2
AzkaIbad <askaibad12@gmail.com>
2023-08-26 13:31:29 +0700

    update

--- app/src/main/java/com/example/coba/NewsService.java
+++ app/src/main/java/com/example/coba/NewsService.java
@@ -5,7 +5,7 @@ import retrofit2.http.GET;
 
 public interface NewsService {
 
-    @GET("v2/top-headlines?country=id&apiKey=e5447b3a0527415f9c4250440adc6d6b")
+    @GET("v2/top-headlines?country=id&apiKey=7cbd04f3e10c4cdbad91029b89148aca")
     Call<NewsResponseModel> newsHeadline();
 
 }
